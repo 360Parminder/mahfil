16d266378fda85b45bbe8391aae7128b64200467
parminder singh <yesask8@gmail.com>
2026-08-11 02:13:39 +0530

    feat: update site metadata and add OpenGraph/Twitter social sharing images

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,34 @@ import "./globals.css";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
-  title: "Mahfil | डीलक्स सैलून",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://mahfil.parminder.pro'),
+  title: "Mahfil | एक सुरीली शाम",
   description: "A nostalgic retro Indian aesthetic lounge player",
   icons: {
     icon: "/icon.png",
     shortcut: "/icon.png",
     apple: "/icon.png",
+  },
+  openGraph: {
+    title: "Mahfil | एक सुरीली शाम",
+    description: "A nostalgic retro Indian aesthetic lounge player",
+    siteName: "Mahfil",
+    images: [
+      {
+        url: "/images/mahfil.png",
+        width: 1200,
+        height: 630,
+        alt: "Mahfil",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mahfil | एक सुरीली शाम",
+    description: "A nostalgic retro Indian aesthetic lounge player",
+    images: ["/images/mahfil.png"],
   },
 };
 
